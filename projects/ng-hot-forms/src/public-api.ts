3a4f67235950401cc9c8a5c77d21250059569ef7
juliancollinson <85016910+juliancollinson@users.noreply.github.com>
2021-05-29 15:11:58 +0200

    Setup/structure (#2)
    * Ignore node modules
    
    * Initialized structure
    
    Co-authored-by: julian000 <j.collinson@sintecnos.com>

--- projects/ng-hot-forms/src/public-api.ts
+++ projects/ng-hot-forms/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ng-hot-forms
  */
 
-export * from './lib/ng-hot-forms.service';
-export * from './lib/ng-hot-forms.component';
-export * from './lib/ng-hot-forms.module';
+export * from './lib/main.module';
